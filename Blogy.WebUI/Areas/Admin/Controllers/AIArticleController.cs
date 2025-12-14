a620b23408c51b3cd8bac298ffdada7a2a0bbf1c
Merve Arpacıoğlu Türk <merveearp@icloud.com>
2025-12-14 18:39:55 +0300

    Added_AI_Article_Service

---
+++ Blogy.WebUI/Areas/Admin/Controllers/AIArticleController.cs
@@ -0,0 +1,39 @@
+﻿using Blogy.Business.DTOs.AIDtos;
+using Blogy.Business.Services.AIServices.ContentService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AIArticleController : Controller
+    {
+        private readonly IAIContentService _aiContentService;
+
+        public AIArticleController(IAIContentService aiContentService)
+        {
+            _aiContentService = aiContentService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateAIArticleDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.Keyword))
+            {
+                ModelState.AddModelError("", "Anahtar kelime boş olamaz.");
+                return View(dto);
+            }
+
+            var result = await _aiContentService.CreateArticleAsync(dto);
+
+            ViewBag.GeneratedArticle = result.Content;
+
+            return View(dto);
+        }
+    }
+}
